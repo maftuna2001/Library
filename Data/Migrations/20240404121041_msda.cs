3f9543a538f0f61fe9e9e9d049ff4e681ee3f5a9
Maftuna <mmakhsudova363@gmail.com>
2024-06-05 14:25:35 +0500

    Добавьте файлы проекта.

---
+++ Data/Migrations/20240404121041_msda.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Data.Migrations
+{
+    public partial class msda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Book");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
